846d1354fd752c22dc2ba439035e46d27a4d097e
Mukesh Jha <mukesh.jha@journey.com>
2021-09-07 22:05:56 +0530

    Initial Commit

---
+++ PtoEmojiBook/PtoEmojiBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PtoEmojiBookApp.swift
+//  PtoEmojiBook
+//
+//  Created by Mukesh Jha on 07/09/21.
+//
+
+import SwiftUI
+
+@main
+struct PtoEmojiBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
