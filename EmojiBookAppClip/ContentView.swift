4f92075bd2798e4e03e91098f9f129204a29fa1d
Mukesh Jha <mukesh.jha@journey.com>
2021-09-07 22:21:41 +0530

    first commit

---
+++ EmojiBookAppClip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiBookAppClip
+//
+//  Created by Mukesh Jha on 07/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiBookAppClip/EmojiBookAppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiBookAppClipApp.swift
+//  EmojiBookAppClip
+//
+//  Created by Mukesh Jha on 07/09/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiBookAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
